c730cd0427d23e3776e51e2719d7703b5fe02d7f
juliemon <155023707+juliemon@users.noreply.github.com>
2023-12-27 17:12:40 -0500

    Add files via upload

---
+++ User.java
@@ -0,0 +1,23 @@
+package com.example.ecoreciclaproyecto;
+
+//clase llamada user para que sea accesible desde otra clase
+public class User {
+    private String username;
+    //accesible solo en esta clase, declarando atributo privado tipo string
+    private String password;
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        //constructor de la clase, publico es llamado cuando se crea una nueva instancia
+        //tiene dos parametros
+    }
+    //getter methods
+    //se declara el metodo el cual devuelve el valor del atributo de la instancia
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
+
